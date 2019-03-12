bfdfd852294ef1e65c57913205281ea9e1ca04c0
Paul Durden <paul.durden@grantstreet.com>
2019-03-12 19:10:20 -0400

    PEX-11289 Initial commit of code and tests

---
+++ lib/HealthCheck/Diagnostic/DBHCheck.pm
@@ -0,0 +1,172 @@
+package HealthCheck::Diagnostic::DBHCheck;
+
+# ABSTRACT: Check a database handle to make sure you have read/write access
+# VERSION
+
+use 5.010;
+use strict;
+use warnings;
+use parent 'HealthCheck::Diagnostic';
+
+use Carp;
+
+sub new {
+    my ($class, @params) = @_;
+
+    # Allow either a hashref or even-sized list of params
+    my %params = @params == 1 && ( ref $params[0] || '' ) eq 'HASH'
+        ? %{ $params[0] } : @params;
+
+    return $class->SUPER::new(
+        label => 'dbh_check',
+        %params
+    );
+}
+
+sub check {
+    my ( $self, %params ) = @_;
+
+    my $dbh = $params{dbh};
+    $dbh ||= $self->{dbh} if ref $self;
+    $dbh = $dbh->(%params) if ref $dbh eq 'CODE';
+
+    croak("Valid 'dbh' is required") unless $dbh and do {
+        local $@; local $SIG{__DIE__}; eval { $dbh->can('ping') } };
+
+    my $read_only  = $params{read_only}  // ((ref $self) && $self->{read_only});
+    my $read_write = $params{read_write} // ((ref $self) && $self->{read_write});
+
+    croak("The 'read_only' and 'read_write' parameters are mutually exclusive")
+        if ($read_only && $read_write);
+
+    my $res = $self->SUPER::check( %params, dbh => $dbh );
+    delete $res->{dbh};    # don't include the object in the result
+
+    return $res;
+}
+
+
+sub _read_write_temp_table {
+    my (%params) = @_;
+    my $dbh      = $params{dbh};
+    my $table    = $params{table_name} // "__DBH_CHECK__";
+    my $status   = "CRITICAL";
+
+    # Drop it like its hot
+    $dbh->do("DROP TABLE IF EXISTS $table");
+
+    $dbh->do(
+        join(
+            "", 
+            "CREATE TEMPORARY TABLE IF NOT EXISTS $table (",
+            "check_id INTEGER PRIMARY KEY,",
+            "check_string VARCHAR(64) NOT NULL",
+            ")"
+        )
+    );
+
+    $dbh->do(
+        join(
+            "",
+            "INSERT INTO $table ",
+            "       (check_id, check_string) ",
+            "VALUES (1,        'Hello world')",
+        )
+    );
+    my @row = $dbh->selectrow_array(
+        "SELECT check_string FROM $table WHERE check_id = 1"
+    );
+
+    $status = "OK" if ($row[0] && ($row[0] eq "Hello world"));
+
+    $dbh->do("DROP TABLE $table");
+
+    return $status;
+}
+
+sub run {
+    my ( $self, %params ) = @_;
+    my $dbh = $params{dbh};
+
+    # Get readonly from parameters or object
+    my $read_only = $params{read_only} // ((ref $self) && $self->{read_only});
+
+    # See if a simple SELECT works
+    my $value  = eval { $dbh->selectrow_array("SELECT 1"); };
+    my $status = (defined($value) && ($value == 1)) ? "OK" : "CRITICAL";
+
+    $status = _read_write_temp_table(%params)
+        if (($status eq "OK") && !$read_only);
+
+    # Generate the human readable info string
+    my $info = sprintf(
+        "%s %s check of %s%s",
+        $status eq "OK" ? "Successful" : "Unsuccessful",
+        $dbh->{Driver}->{Name},
+        $dbh->{Name},
+        $dbh->{Username} ? " as $dbh->{Username}" : "",
+    );
+
+    return { status => $status, info => $info };
+}
+
+1;
+__END__
+
+=head1 SYNOPSIS
+
+    my $health_check = HealthCheck->new( checks => [
+        HealthCheck::Diagnostic::DBHCheck->new(
+            dbh => \&connect_to_read_write_db,
+            tags => [qw< dbh_check_rw >]
+        ),
+        HealthCheck::Diagnostic::DBHCheck->new(
+            dbh => \&connect_to_read_only_db,
+            tags
+        ),
+    ] );
+
+    my $result = $health_check->check;
+    $result->{status}; # OK on a successful ping or CRITICAL otherwise
+
+=head1 DESCRIPTION
+
+Determines if the database can be used for read and write access, or read only
+access.
+
+For C<readonly> access, a simple SELECT statement is used.
+
+For C<reaad_write> access, a temporary table is created, and used for testing.
+
+=head1 ATTRIBUTES
+
+Those inherited from L<HealthCheck::Diagnostic/ATTRIBUTES> plus:
+
+=head2 dbh
+
+A coderef that returns a
+L<DBI database handle object|DBI/DBI-DATABSE-HANDLE-OBJECTS>
+or optionally the handle itself.
+
+Can be passed either to C<new> or C<check>.
+
+=head2 read_only
+
+A boolean value indicating if only the read access should be tested.
+B<NOTE:> Cannot be used when C<read_write> is true.
+
+=head2 read_write
+
+A boolean value indicating if boteh read and write access should be tested.
+B<NOTE:> Cannot be used when C<read_only> is true.
+
+=head1 DEPENDENCIES
+
+L<HealthCheck::Diagnostic>
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+None
+
+=cut
+
---
+++ t/HealthCheck-Diagnostic-DBHCheck.t
@@ -0,0 +1,73 @@
+use strict;
+use warnings;
+use Test::More;
+
+use HealthCheck::Diagnostic::DBHCheck;
+use DBI;
+use DBD::SQLite;
+
+my $bad_dbh = qr/Valid 'dbh' is required at \S+ line \d+/;
+
+eval { HealthCheck::Diagnostic::DBHCheck->check };
+like $@, $bad_dbh, "Expected error with no DBHi (as class)";
+
+eval { HealthCheck::Diagnostic::DBHCheck->new->check };
+like $@, $bad_dbh, "Expected error with no DBH";
+
+eval { HealthCheck::Diagnostic::DBHCheck->new( dbh => {} )->check };
+like $@, $bad_dbh, "Expected error with DBH as empty hashref";
+
+eval { HealthCheck::Diagnostic::DBHCheck->check( dbh => bless {} ) };
+like $@, $bad_dbh, "Expected error with DBH as empty blessed hashref";
+
+eval { HealthCheck::Diagnostic::DBHCheck->check( dbh => sub {} ) };
+like $@, $bad_dbh, "Expected error with DBH empty sub";
+
+my $dbname = 'dbname=:memory:';
+my $dbh = DBI->connect("dbi:SQLite:$dbname","","", { PrintError => 0 });
+
+eval {
+        HealthCheck::Diagnostic::DBHCheck->new(
+            dbh        => $dbh,
+            read_only  => 1,
+            read_write => 1
+        )->check;
+    };
+
+like
+    $@,
+    qr/mutually exclusive/,
+    "Expected error with both read_only and read_write";
+
+is_deeply( HealthCheck::Diagnostic::DBHCheck->new( dbh => $dbh )->check, {
+    label  => 'dbh_check',
+    status => 'OK',
+    info   => "Successful SQLite check of $dbname",
+}, "OK status as expected" );
+
+$dbh->disconnect;
+is_deeply( HealthCheck::Diagnostic::DBHCheck->check( dbh => $dbh ), {
+    status => 'CRITICAL',
+    info   => "Unsuccessful SQLite check of dbname=:memory:",
+}, "CRITICAL status as expected" );
+
+# Now try it with a username and a coderef
+$dbh = sub {
+    DBI->connect("dbi:SQLite:$dbname","FakeUser","", { PrintError => 0 })
+};
+
+is_deeply( HealthCheck::Diagnostic::DBHCheck->new( dbh => $dbh )->check, {
+    label  => 'dbh_check',
+    status => 'OK',
+    info   => "Successful SQLite check of $dbname as FakeUser",
+}, "OK status as expected" );
+
+# Turn it into a coderef that returns a disconnected dbh
+$dbh = do { my $x = $dbh; sub { my $y = $x->(); $y->disconnect; $y } };
+
+is_deeply( HealthCheck::Diagnostic::DBHCheck->check( dbh => $dbh ), {
+    status => 'CRITICAL',
+    info   => "Unsuccessful SQLite check of $dbname as FakeUser",
+}, "CRITICAL status as expected" );
+
+done_testing;
